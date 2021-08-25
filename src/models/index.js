fe2a2129aef3c5b6935fff3bad4321993c0124de
mamoun kamal alshishani <mamoun.alshishani@yahoo.com>
2021-08-25 14:40:30 +0300

    update

--- src/models/index.js
+++ src/models/index.js
@@ -7,7 +7,7 @@
 
 const { Sequelize, DataTypes } = require('sequelize');
 const UserSchema = require('./user_model');
-const sequelize = new Sequelize('postgres://postgres@localhost:5432/socialproject');
+const sequelize = new Sequelize('postgres://postgres:0000@localhost:5432/socialproject');
 
 
 const Users = UserSchema(sequelize, DataTypes);
--- src/routers/end_Points.js
+++ src/routers/end_Points.js
@@ -5,8 +5,9 @@ const router = express.Router();
 const {Users} = require('../models/index');
 router.use(express.json())
 
-
-
+const bearer=require('../middleWare/berear_auth')
+const basic=require('../middleWare/basic_auth')
+const asl=require('../middleWare/acl')
 
 function signUp(req,res){
     console.log(req)
@@ -16,14 +17,41 @@ function signUp(req,res){
     .catch(err => res.status(400).send(err))
 }
 
+function readHandler(req,res){
+
+res.send('read successful')
+
+
+};
+
+
+function writeHandler(req,res){
+
+    res.send('write successful')
+
+    
+};
+
+
+function deleteHandler(req,res){
+
+
+    res.send('delete successful')
+    
+};
+
 
 function signIn(req,res){
     res.status(200).send(req.user);
 }
 
 
-router.get('/signin', signIn)
+router.post('/signin', basic(Users) ,signIn)
 router.post('/signUp', signUp)
+router.get('/read', bearer(Users),asl("read"),readHandler)
+router.put('/write', bearer(Users),asl("write"),writeHandler)
+router.delete('/delete',bearer(Users),asl("delete"), deleteHandler)
+
 
 
 module.exports = router
